442f3be5b5083960ce001d91013091341b0208b5
TerminalX2 <124107188+TerminalX2@users.noreply.github.com>
2024-03-05 08:24:54 -0800

    updated computePID, added drive/turn PID functions

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@
 # ---------------------------------------------------------------------------- #
 
 # Library imports
-from math import pi
+from math import pi, fmod, sin, cos, atan2, hypot
 from vex import *
 
 #---------------------------------------------------------------------------#
@@ -22,8 +22,8 @@ brain = Brain()
 controller = Controller(PRIMARY)
 
 # Add motors here
-drive_LF = Motor(Ports.PORT1, GearSetting.RATIO_6_1, True)
-drive_LB = Motor(Ports.PORT2, GearSetting.RATIO_6_1, True)
+drive_LF = Motor(Ports.PORT1, GearSetting.RATIO_6_1, False)
+drive_LB = Motor(Ports.PORT2, GearSetting.RATIO_6_1, False)
 drive_RF = Motor(Ports.PORT3, GearSetting.RATIO_6_1, True)
 drive_RB = Motor(Ports.PORT4, GearSetting.RATIO_6_1, True)
 placeholder_motor_5 = Motor(Ports.PORT5, GearSetting.RATIO_6_1, True)
@@ -92,93 +92,40 @@ def pre_auton():
 #---------------------------------------------------------------------------#
 #                               PID Functions                               #
 #---------------------------------------------------------------------------#
+        
+def set_constants(maxVoltage, kP, kI, kD, timeout_msec):
+    global dt, kp, ki, kd, max_voltage, output, prev_error, timeout
+    global tolerance
 
-def drive_distance(distance):
-
-    drive.reset_position()
-
-    # Tuning parameters
+    max_voltage = maxVoltage
+    kp = kP
+    ki = kI
+    kd = kD
+    timeout = timeout_msec 
     dt = 20
-    kp, ki, kd = 0, 0, 0
     tolerance = 0.5
-    timeout = abs(distance) / 2
+        
+def compute(target, sensor):
+    global error, integral, derivative, dt, kp, ki, kd, max_voltage, output
+    global prev_error, prev_deriv, acceleration, tolerance, timeout
 
-    error = distance - drive.position()
-    integral, acceleration, prev_deriv, time_spent_running = 0, 0, 0, 0
-    prev_error = error
+    error = target - sensor
+    
+    integral += error * dt
+    integral = clamp(integral, 5, -5)
 
-    while abs(error) > tolerance:
-
-        error = distance - drive.position()
-
-        integral += error * dt
-        integral = clamp(integral, 5, -5) 
-        # This statement restricts the integral between -5 and 5
-        # to prevent integral windup.
-
-        derivative = (error - prev_error) * dt
-        if abs(derivative) > abs(acceleration + prev_deriv) and prev_deriv != 0:
+    derivative = (error - prev_error) / dt
+    if abs(derivative) > abs(acceleration + prev_deriv) and prev_deriv != 0:
             derivative = acceleration + prev_deriv 
-            # This statement allows smooth derivative calculations.
 
-        output = kp * error + ki * integral + kd * derivative
-        output = clamp(output, 12, -12) 
-        drive.spin(FORWARD, output, VOLT)
+    output = kp * error + ki * integral + kd * derivative
+    output = clamp(output, 12, -12)
 
-        wait(dt)
-        time_spent_running += dt
-        if time_spent_running > timeout:
-            break
-
-        acceleration = derivative - prev_deriv
-        prev_deriv = derivative
-        prev_error = error
-
-    drive.stop()
-
-def turn_to_angle(angle):
-
-    drive.reset_position()
-
-    # Tuning parameters
-    dt = 20
-    kp, ki, kd = 0, 0, 0
-    tolerance = 0.5
-    timeout = 1000
-
-    error = angle - inertial.rotation()
-    integral, acceleration, prev_deriv, time_spent_running = 0, 0, 0, 0
+    acceleration = derivative - prev_deriv
+    prev_deriv = derivative
     prev_error = error
-
-    while abs(error) > tolerance:
-
-        error = angle - inertial.rotation()
-
-        integral += error * dt
-        integral = clamp(integral, 5, -5) 
-        # This statement restricts the integral between -5 and 5
-        # to prevent integral windup.
-
-        derivative = (error - prev_error) * dt
-        if abs(derivative) > abs(acceleration + prev_deriv) and prev_deriv != 0:
-            derivative = acceleration + prev_deriv 
-            # This statement allows smooth derivative calculations.
-
-        output = kp * error + ki * integral + kd * derivative
-        output = clamp(output, 12, -12) 
-        drive_L.spin(FORWARD, output, VOLT)
-        drive_R.spin(REVERSE, output, VOLT)
-
-        wait(dt)
-        time_spent_running += dt
-        if time_spent_running > timeout:
-            break
-
-        acceleration = derivative - prev_deriv
-        prev_deriv = derivative
-        prev_error = error
-
-    drive.stop()
+    
+    return output
 
 # ---------------------------------------------------------------------------#
 #                                                                            #
@@ -198,6 +145,94 @@ def auto_defense_side():
 
 def auto_skills():
     pass
+
+#---------------------------------------------------------------------------#
+#                        Autonomous Selector Functions                      #
+#---------------------------------------------------------------------------#
+
+autons = ["Skills", "Offense Side", "Defense Side", "Test", "Calibration"]
+
+debounce = True
+def debounce_true():
+    global debounce
+    wait(500,MSEC)
+    debounce = True
+
+def autonomous_menu():
+    global debounce
+    position = 0
+    while 1:
+        controller.screen.clear_screen()
+        controller.screen.set_cursor(1, 1)
+        controller.screen.print(f"Selecting: {str(position + 1)}. {autons[position]}")
+        controller.screen.next_row()
+        controller.screen.print("A to select auton")
+        
+        if controller.buttonRight.pressing() and debounce:
+            debounce = False
+            if position + 1 != len(autons):
+                position += 1
+        Thread(debounce_true)
+        
+        if controller.buttonLeft.pressing() and debounce:
+            debounce = False
+            if position != 0:
+                position -= 1
+        Thread(debounce_true)
+        
+        if controller.buttonA.pressing():
+            return autons[position]
+        
+        wait(100, MSEC)
+
+def select_autonomous():
+    global at_competition
+
+    driver_control = False
+    controller.screen.clear_screen()
+    controller.screen.set_cursor(1, 1)
+    controller.screen.print("Are you at a comp?")
+    controller.screen.next_row()
+    controller.screen.print("A = Yes")
+    controller.screen.next_row()
+    controller.screen.print("B = No, Y = Driver Control")
+
+    while 1:
+        if controller.buttonA.pressing():
+            at_competition = True
+            break
+
+        if controller.buttonB.pressing():
+            at_competition = False
+            break
+
+        if controller.buttonY.pressing():
+            driver_control = True
+            break
+
+    if driver_control:
+        user_control()
+
+    wait(500, MSEC)
+    auton = autonomous_menu()
+
+    # controller.screen.clear_screen()
+    # controller.screen.set_cursor(1,1)
+    # controller.screen.print("Run \"" + str(auton) + "\"?")
+    # controller.screen.next_row()
+    # controller.screen.print("A = Confirm")
+    # controller.screen.next_row()
+    # controller.screen.print("B = Choose again")
+   
+    # while 1:
+    #     if controller.buttonB.pressing():
+    #         select_autonomous()
+
+            
+    #     if controller_1.buttonA.pressing():
+            
+    #         return auton
+    #     wait(50,MSEC)
 
 def autonomous():
     # ..........................................................................
@@ -221,11 +256,114 @@ def drive_with_voltage(left_voltage, right_voltage):
     drive_L.spin(FORWARD, left_voltage, VOLT)
     drive_R.spin(FORWARD, right_voltage, VOLT)
 
+def drive_distance(distance):
+    global error, integral, derivative, dt, kp, ki, kd, max_voltage, output
+    global prev_error, prev_deriv, acceleration, tolerance, timeout
+    
+    drive.reset_position()
+    position = (drive_L.position() + drive_R.position()) / 2
+    error = distance - position
+    integral, acceleration, prev_deriv, time_spent_running = 0, 0, 0, 0
+    prev_error = error
+
+    # Tuning parameters: maxVoltage, kP, kI, kD, timeout (msec)
+    set_constants(12, 0, 0, 0, 5000)
+    
+    while True:
+        drive_output = compute(distance, position)
+        drive_with_voltage(drive_output, drive_output)
+        if abs(error) < tolerance:
+            integral = 0
+            output = 0
+            break
+        wait(dt, MSEC)
+        time_spent_running += dt
+        if time_spent_running > timeout:
+            break
+    drive.stop()
+
+def turn_to_angle(angle):
+    global error, integral, derivative, dt, kp, ki, kd, max_voltage, output
+    global prev_error, prev_deriv, acceleration, tolerance, timeout
+
+    rotation = inertial.rotation()
+    error = angle - rotation
+    integral, acceleration, prev_deriv, time_spent_running = 0, 0, 0, 0
+    prev_error = error
+
+    # Tuning parameters: maxVoltage, kP, kI, kD, timeout (msec)
+    set_constants(12, 0, 0, 0, 3000)
+
+    while True:
+        turn_output = compute(angle, rotation)
+        drive_with_voltage(turn_output, -turn_output)
+        if abs(error) < tolerance:
+            integral = 0
+            output = 0
+            break
+        wait(dt, MSEC)
+        time_spent_running += dt
+        if time_spent_running > timeout:
+            break
+    drive.stop()
+
+def left_swing_to_angle(angle):
+
+    global error, integral, derivative, dt, kp, ki, kd, max_voltage, output
+    global prev_error, prev_deriv, acceleration, tolerance, timeout
+
+    rotation = reduce_angle(-180, 180, inertial.rotation())
+    error = angle - rotation
+    integral, acceleration, prev_deriv, time_spent_running = 0, 0, 0, 0
+    prev_error = error
+
+    # Tuning parameters: maxVoltage, kP, kI, kD, timeout (msec)
+    set_constants(12, 0, 0, 0, 3000)
+
+    while True:
+        swing_turn_output = compute(angle, rotation)
+        # Only the left side of the drive turns, so this is a "left swing".
+        drive_with_voltage(swing_turn_output, 0)
+
+        if abs(error) < tolerance:
+            integral = 0
+            output = 0
+            break
+        wait(dt, MSEC)
+        time_spent_running += dt
+        if time_spent_running > timeout:
+            break
+    drive.stop()
+
+def right_swing_to_angle(angle):
+
+    global error, integral, derivative, dt, kp, ki, kd, max_voltage, output
+    global prev_error, prev_deriv, acceleration, tolerance, timeout
+
+    rotation = reduce_angle(-180, 180, inertial.rotation())
+    error = angle - rotation
+    integral, acceleration, prev_deriv, time_spent_running = 0, 0, 0, 0
+    prev_error = error
+
+    # Tuning parameters: maxVoltage, kP, kI, kD, timeout (msec)
+    set_constants(12, 0, 0, 0, 3000)
+
+    while True:
+        swing_turn_output = compute(angle, rotation)
+        # Only the right side of the drive turns, so this is a "right swing".
+        drive_with_voltage(0, swing_turn_output) 
+        
+        if abs(error) < tolerance:
+            integral = 0
+            output = 0
+            break
+        wait(dt, MSEC)
+        time_spent_running += dt
+        if time_spent_running > timeout:
+            break
+    drive.stop()
+
 # COMING FUNCTIONS
-# drive_distance()
-# turn_to_angle()
-# left_swing_to_angle()
-# right_swing_to_angle()
 # drive_to_point()
 # turn_to_point()
 # turn_along_arc()
@@ -273,28 +411,39 @@ def user_control():
         # update your motors, etc.
         # ........................................................................
 
-        # Replace this line with control_tank() for tank drive 
-        # or control_holonomic() for holomonic drive
+        # Replace this line with control_tank() for tank drive or
+        # control_holonomic() for holo drive.
         control_arcade()
 
         wait(20, MSEC) # Sleep the task for a short amount of time to
                        # prevent wasted resources.
-        
+
 #---------------------------------------------------------------------------#
 #                               Button Macros                               #
 #---------------------------------------------------------------------------#
 
-def macro_button_L1():
+def macro_buttonL1():
     # Insert code here and remove the 'pass' keyword
     pass
     
-def macro_button_L2():
+def macro_buttonL2():
+    # Insert code here and remove the 'pass' keyword
+    pass
+
+def macro_buttonR1():
+    # Insert code here and remove the 'pass' keyword
+    pass
+    
+def macro_buttonR2():
     # Insert code here and remove the 'pass' keyword
     pass
 
 # Set up callbacks for autonomous and driver control periods.
-controller.buttonL1.pressed(macro_motor_5_fwd)
-controller.buttonL2.pressed(macro_motor_5_rev)
+controller.buttonL1.pressed(macro_buttonL1)
+controller.buttonL2.pressed(macro_buttonL2)
+
+controller.buttonR1.pressed(macro_buttonR1)
+controller.buttonR2.pressed(macro_buttonR2)
 
 # A global instance of competition
 competition = Competition(user_control, autonomous)
